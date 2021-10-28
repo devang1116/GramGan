665eafc497e0c61213d174ddd840261257b4b30f
Devang <papinwardevang@gmail.com>
2021-10-28 16:51:17 +0530

    Initial Commit

---
+++ social media/social_mediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  social_mediaApp.swift
+//  social media
+//
+//  Created by Devang Papinwar on 28/10/21.
+//
+
+import SwiftUI
+
+@main
+struct social_mediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ social mediaTests/social_mediaTests.swift
@@ -0,0 +1,33 @@
+//
+//  social_mediaTests.swift
+//  social mediaTests
+//
+//  Created by Devang Papinwar on 28/10/21.
+//
+
+import XCTest
+@testable import social_media
+
+class social_mediaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
